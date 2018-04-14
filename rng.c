a7af67822c79eeeb1722f8a9b4463c046a2d9bac
Irwin1138 <irwin1139@gmail.com>
2018-04-15 02:49:20 +0300

    added licenses and descriptions of rng algos used

--- rng.c
+++ rng.c
@@ -28,6 +28,23 @@ static __inline uint64_t rotl(const uint64_t x, int k) {
 	return (x << k) | (x >> (64 - k));
 }
 
+/*  Written in 2015 by Sebastiano Vigna (vigna@acm.org)
+
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+/* This is a fixed-increment version of Java 8's SplittableRandom generator
+   See http://dx.doi.org/10.1145/2714064.2660195 and 
+   http://docs.oracle.com/javase/8/docs/api/java/util/SplittableRandom.html
+
+   It is a very fast generator passing BigCrush, and it can be useful if
+   for some reason you absolutely want 64 bits of state; otherwise, we
+   rather suggest to use a xoroshiro128+ (for moderately parallel
+   computations) or xorshift1024* (for massively parallel computations)
+   generator. */
+/* The state can be seeded with any value. */
 uint64_t splitmix(uint64_t x) {
 	uint64_t z = (x += 0x9e3779b97f4a7c15);
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
@@ -39,7 +56,37 @@ void setseed(XoroshiroSeed *seed, uint64_t newseed) {
 	seed->s0 = splitmix(newseed);
 	seed->s1 = splitmix(newseed);
 }
+/*  Written in 2016 by David Blackman and Sebastiano Vigna (vigna@acm.org)
 
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+/* This is the successor to xorshift128+. It is the fastest full-period
+   generator passing BigCrush without systematic failures, but due to the
+   relatively short period it is acceptable only for applications with a
+   mild amount of parallelism; otherwise, use a xorshift1024* generator.
+
+   Beside passing BigCrush, this generator passes the PractRand test suite
+   up to (and included) 16TB, with the exception of binary rank tests, as
+   the lowest bit of this generator is an LFSR of degree 128. The next bit
+   can be described by an LFSR of degree 8256, but in the long run it will
+   fail linearity tests, too. The other bits needs a much higher degree to
+   be represented as LFSRs.
+
+   We suggest to use a sign test to extract a random Boolean value, and
+   right shifts to extract subsets of bits.
+
+   Note that the generator uses a simulated rotate operation, which most C
+   compilers will turn into a single instruction. In Java, you can use
+   Long.rotateLeft(). In languages that do not make low-level rotation
+   instructions accessible xorshift128+ could be faster.
+
+   The state must be seeded so that it is not everywhere zero. If you have
+   a 64-bit seed, we suggest to seed a splitmix64 generator and use its
+   output to fill s. */
 uint64_t next(XoroshiroSeed *seed) {
 	const uint64_t s0 = seed->s0;
 	uint64_t s1 = seed->s1;
